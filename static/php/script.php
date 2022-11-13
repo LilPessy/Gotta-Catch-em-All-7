073de6b1ac3b7c70ad13f2ce09321d21d5760c83
Piergiusto Ranieri <74157329+PiergiustoRanieri@users.noreply.github.com>
2022-11-13 18:29:12 +0100

    Fix Struttura #33dy8un

--- static/php/script.php
+++ static/php/script.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acquisizione Pokedex</title>
+</head>
+<body>
+
 <?php>
 
 //acquisizione del file
@@ -15,3 +25,10 @@ $rows = file($file);
 foreach($rows as $row) {
     $columns = explode(';', $row);
 }
+?>
+
+<div> 
+</div>
+</body>
+</html>
+
