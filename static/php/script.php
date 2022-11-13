e272b0b70f89353210958fc566b95e332b7f6d5b
Piergiusto Ranieri <74157329+PiergiustoRanieri@users.noreply.github.com>
2022-11-13 18:00:52 +0100

    Creazione Tabella #33dy8un
    Aggiunto un controllo sulla lettura del file e la creazione di una tabella con tutti i nostri dati del pokedex

--- static/php/script.php
+++ static/php/script.php
@@ -1,5 +1,17 @@
 <?php>
 
+//acquisizione del file
 $filename = 'pokedex.csv';
 $path = '../data/pokedex.csv';
 $file = $path . $filename;
+
+//controllo
+if(!is_readable($file)) {
+    die('File non leggibile o non presente');
+}
+$rows = file($file);
+
+//separazione delle colonne in un array di stringhe
+foreach($rows as $row) {
+    $columns = explode(';', $row);
+}
