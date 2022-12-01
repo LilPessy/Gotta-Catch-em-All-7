577a7790e34a33873b91bf5bac476254d69631c1
Piergiusto Ranieri <74157329+PiergiustoRanieri@users.noreply.github.com>
2022-12-01 18:59:13 +0100

    Ricreazione funzione 1 #33dy8un
    +correzione html

--- php/script.php
+++ php/script.php
@@ -1,67 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acquisizione Pokedex</title>
-</head>
-<body>
-
 <?php
+    if(isset($_POST['HP']) && isset($_POST['ATK']) && isset($_POST['DEF']) && isset($_POST['SPATK']) && isset($_POST['SPDEF']) && isset($_POST['SPEED'])){
+        $HP = $_POST["HP"];
+        $ATK = $_POST["ATK"];
+        $DEF = $_POST["DEF"];
+        $SPATK = $_POST["SPATK"];
+        $SPDEF = $_POST["SPDEF"];
+        $SPEED = $_POST["SPEED"];
+    }
 
-//acquisizione del file
-$filename = 'pokedex.csv';
-$path = '../data/';
-$file = $path . $filename;
-
-//controllo
-if(!is_readable($file)) {
-    die('File non leggibile o non presente');
-}
-$rows = file($file);
-
+    function readcsv(){
+        $file = fopen("../data/pokedex.csv", "r");
+        $data = array();
+        while (($line = fgetcsv($file)) !== FALSE) {
+            $data[] = $line;
+        }
+        fclose($file);     
+        return $data; 
+    }
+    $csv = readcsv();
+    print_r ($csv);
+    echo "cazzo";
+    
+    
 ?>
-<div class="pokemonList">
-    <?php
-    $pokedex = [];
-    foreach($rows as $row) {     
-                                   //separazione delle colonne in un array di stringhe
-        array_push($pokedex, $row);
-    }
-    array_splice($pokedex, 0, 1); //toglie l'intestazione
-
-    //echo($pokedex[0]);
-
-    foreach($pokedex as $pokemon) {     
-                                  //qui farai l'array associativo
-                                  $value = explode(",", $pokemon);
-                                  echo($value[2]);
-    }
-
-    ?>
-        <div class="pokemonCard">
-        <?php           
-            /*print($columns[1] . "\t"); // nome
-            print($columns[2]); // tipo principale
-            print($columns[3]); // tipo principale
-            print($columns[9]); // tipo principale*/
-            //$array = [NameType 1Type 2TotalHPAttackDefenseSp. AtkSp. DefSpeedGenerationLegendary]
-            
-            /*foreach($columns as $col){      //ciclare per stampare tutte le categorie del pokedex 
-                print($col);
-            }*/
-            
-        ?>
-        </div>
-</div>
-
-<div class="getForm">
-    <?php
-        //$hp = $_POST["HP"];
-
-    ?>
-</div>
-</body>
-</html>
 
